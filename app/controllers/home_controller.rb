710fd73e3d7dc9cf276ebd0228acc3c5ba93b3cd
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-01-05 04:44:27 +0900

    [update] QRコード生成実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,19 @@ class HomeController < ApplicationController
     @orders = @current_user.orders.order(id: :desc)
   end
 
+  def qrcode
+    @amount = qrcode_params['amount'].to_i
+    unpack_str = "#{@amount}->#{current_user.id}".unpack('H*').first
+    @qrcode_text = "wao://#{unpack_str}"
+  end
+
   def grant_login_bonus
     @current_user.grant_login_bonus
+  end
+
+  private
+
+  def qrcode_params
+    params.require(:qrcode).permit(:amount)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
-  def qrcode_tag(text, options = {})
-    ::RQRCode::QRCode.new(text).as_svg(options).html_safe
+  def qrcode_tag(text)
+    qr = ::RQRCode::QRCode.new(text)
+    qr_data_url = ChunkyPNG::Image.from_datastream(qr.as_png.resize(500, 500).to_datastream).to_data_url
+    image_tag(qr_data_url, width: '100%')
   end
 end
