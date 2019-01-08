72f19413b98f6dbc7583221ebeca1d692298008d
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-01-09 02:11:52 +0900

    [update] バリデーションを追加したので、管理者ユーザーを登録するときのみバリデーションを無効に

--- db/migrate/20190104155111_create_admin_users_to_users.rb
+++ db/migrate/20190104155111_create_admin_users_to_users.rb
@@ -1,8 +1,8 @@
 class CreateAdminUsersToUsers < ActiveRecord::Migration[5.1]
   def up
-    User.create(id: -1, name: "wao:PAY運営", username: "登録ボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar')
-    User.create(id: -2, name: "wao:PAY運営", username: "ログインボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar')
-    User.create(id: -3, name: "wao:PAY運営", username: "お友達紹介ボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar')
+    User.new(id: -1, name: "wao:PAY運営", username: "登録ボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar').save(validate: false)
+    User.new(id: -2, name: "wao:PAY運営", username: "ログインボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar').save(validate: false)
+    User.new(id: -3, name: "wao:PAY運営", username: "お友達紹介ボーナス", password: 'hogefoobar', password_confirmation: 'hogefoobar').save(validate: false)
   end
 
   def down
