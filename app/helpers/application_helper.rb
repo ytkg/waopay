1c1882a3d8c4b7e5410b48899a450d83a230df1f
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-01-12 14:01:24 +0900

    [update] qrコードの大きさが大きい気がしたので、100%表示から80%表示変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def qrcode_tag(text)
     qr = ::RQRCode::QRCode.new(text)
     qr_data_url = ChunkyPNG::Image.from_datastream(qr.as_png.resize(500, 500).to_datastream).to_data_url
-    image_tag(qr_data_url, width: '100%')
+    image_tag(qr_data_url, width: '80%')
   end
 end
